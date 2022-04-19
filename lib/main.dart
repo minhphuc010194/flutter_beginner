a8eebfaa3c5896c50dbf13c799f00933a34ac66a
Châu Minh Phúc <58540065+minhphuc010194@users.noreply.github.com>
2022-04-19 23:07:15 +0700

    create an infinite scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+    return const MaterialApp(
+      title: 'random text list',
+      home: RandomWords(),
     );
   }
 }
--- lib/pages/randomWords.dart
+++ lib/pages/randomWords.dart
@@ -9,9 +9,27 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (context, i) {
+            if (i.isOdd) return const Divider();
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            return ListTile(
+              title: Text(
+                index.toString() + '. ' + _suggestions[index].asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          }),
+    );
   }
 }
